cad401429d56de98b27eb3aa7feed31f7b6f035f
Theodor Skippervold <theodorskippervold@Theodors-MacBook-Pro.local>
2017-05-20 13:56:17 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Ethereum miner for Microsoft Azure
-# Script by Ben Thompson
 echo -e "\e[31;43m***** Installing Drivers and Prerequisites *****\e[0m"
 sudo apt-get install software-properties-common gcc make
 sudo add-apt-repository ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum ethminer geth build-essential
-wget http://uk.download.nvidia.com/XFree86/Linux-x86_64/375.20/NVIDIA-Linux-x86_64-375.20.run
-sudo chmod +x NVIDIA-Linux-x86_64-375.20.run
-sudo ./NVIDIA-Linux-x86_64-375.20.run --accept-license --no-questions --no-install-compat32-libs
-echo -e "\e[31;43m***** Generating Wallet: *****\e[0m"
-geth account new
+
+wget -O /tmp/NVIDIA-Linux-x86_64-367.48.run https://go.microsoft.com/fwlink/?linkid=836899
+sudo apt install gcc
+sudo apt install make
+cd /tmp
+chmod +x NVIDIA-Linux-x86_64-367.48.run
+sudo sh ./NVIDIA-Linux-x86_64-367.48.run
+
+echo -e "\e[31;43m***** Do 'geth account new' to create a new wallet *****\e[0m"
 echo -e "\e[31;43m***** Done! To start mining do: 'ethminer -G -F YOURPOOL --farm-recheck 200' *****\e[0m"
